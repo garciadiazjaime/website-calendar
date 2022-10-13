60fd902c30321caa9cf04392443c11b1f4721ed3
jaime <garciadiazjaime@gmail.com>
2022-10-13 13:16:55 -0500

    feat: init

---
+++ pages/index.js
@@ -0,0 +1,174 @@
+import { useState, useRef } from 'react';
+import Head from 'next/head'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const places = [
+  {
+    id: 1,
+    title: 'Cabain #1'
+  },
+  {
+    id: 2,
+    title: 'Cabain #2'
+  },
+  {
+    id: 3,
+    title: 'Cabain #3'
+  },
+]
+
+export default function Home() {
+  const [selectedPlace, setSelectedPlace] = useState({})
+  const [checkIn, setCheckIn] = useState(new Date());
+  const [checkOut, setCheckOut] = useState(new Date());
+  const nameInputRef = useRef(null);
+
+
+  const placeClickHandler = (id) => {
+    const newState = {
+      ...selectedPlace
+    }
+    newState[id] = newState[id] === undefined ? true : !newState[id]
+
+    setSelectedPlace(newState)
+  }
+
+  const reserveClickHandler = () => {
+    const payload = {
+      place: selectedPlace,
+      checkIn,
+      checkOut,
+      name: nameInputRef.current.value,
+    }
+
+    console.log(payload)
+  }
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+
+      </header>
+      <main>
+        <div className='place-selection'>
+          {
+            places.map(place => (
+              <div onClick={() => placeClickHandler(place.id)} className={selectedPlace[place.id] ? 'place-selected' : ''}>
+                {place.title}
+              </div>  
+            ))
+          }
+        </div>
+
+        <div className='dates-selection'>
+          <div>
+            Check-in: <br />
+            <Calendar className="calendar" onChange={setCheckIn} value={checkIn} />
+          </div>
+          <br />
+          <div>
+            Check-out: <br />
+            <Calendar className="calendar" onChange={setCheckOut} value={checkOut} />
+          </div>
+        </div>
+
+        <div className='contact-reservation'>
+          <div>
+            Name <br />
+            <input type="text" ref={nameInputRef} />
+          </div>
+        </div>
+
+
+        <div className='control'>
+          <div className='btn' onClick={reserveClickHandler}>Reserve</div>
+        </div>
+        
+      </main>
+
+      <footer>
+
+      </footer>
+
+      <style jsx>{`
+        header, footer {
+          height: 120px;
+          border: 1px solid;
+        }
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+        }
+
+        .place-selection {
+          margin-top: 20px;
+          display: flex;
+        }
+
+        .place-selection div {
+          padding: 30px;
+          flex: 1;
+          border: 1px solid;
+          text-align: center;
+        }
+
+        .place-selected {
+          background: lightblue;
+        }
+
+        .dates-selection {
+          padding: 30px 0;
+        }
+
+        .contact-reservation input {
+          padding: 12px;
+          font-size: 40px;
+          width: 100%;
+        }
+
+        .control {
+          padding: 30px 0;
+        }
+
+        .btn {
+          padding: 30px;
+          font-size: 40px;
+          text-align: center;
+          border: 1px solid;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+
+        .calendar {
+          width: 100%;
+        }
+      `}</style>
+    </div>
+  )
+}
