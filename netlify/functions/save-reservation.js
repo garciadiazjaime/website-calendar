b2b18fc1ab1f4626a88d10c6bdb35a63a2d13508
jaime <garciadiazjaime@gmail.com>
2022-10-15 00:25:32 -0500

    feat: adjust envs

--- netlify/functions/save-reservation.js
+++ netlify/functions/save-reservation.js
@@ -2,8 +2,8 @@ const AWS = require("aws-sdk");
 
 AWS.config.update({
   region: "us-east-1",
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
 });
 
 const documentClient = new AWS.DynamoDB.DocumentClient();
